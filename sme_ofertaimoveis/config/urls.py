da624d00347c55b656be682b2857f4bc998e4ce0
Calvin Rossinhole <calvin.masters@gmail.com>
2020-12-03 13:49:03 -0300

    comenta admin em url

--- sme_ofertaimoveis/config/urls.py
+++ sme_ofertaimoveis/config/urls.py
@@ -2,7 +2,7 @@ import environ
 from des import urls as des_urls
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib import admin
+# from django.contrib import admin
 from django.urls import include, path
 from django.views import defaults as default_views
 from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
@@ -23,7 +23,7 @@ urlpatterns = [
     path("docs/", schema_view, name="docs"),
     path("django-des/", include(des_urls)),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
-    path("admin/", admin.site.urls),
+    # path("admin/", admin.site.urls),
     path("api-token-auth/", obtain_jwt_token),
     path("api-token-refresh/", refresh_jwt_token),
     path("login/", LoginView.as_view()),
