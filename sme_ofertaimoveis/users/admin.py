e8ee6bae229e9fa66e349283be05bfcab6fcd94b
Calvin Rossinhole <calvin.masters@gmail.com>
2020-12-07 17:17:44 -0300

    permissionamento - modelagem perfil e secretaria

--- sme_ofertaimoveis/users/admin.py
+++ sme_ofertaimoveis/users/admin.py
@@ -1,6 +1,30 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 from django.contrib.auth import get_user_model
+from .models import Perfil
 
 User = get_user_model()
 
-admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "perfil")
+
+    def save_model(self, request, obj, form, change):
+        error = False
+        if (change and
+                'perfil' in form.changed_data):
+            if (Perfil.objects.get(id=form.data.get('perfil')).nome == 'ADMIN' and
+                    User.objects.filter(perfil__nome='ADMIN').count() == 3):
+                messages.set_level(request, messages.ERROR)
+                messages.error(request, 'Excedeu o limite de usuários ADMIN no sistema.')
+                error = True
+            elif (Perfil.objects.get(id=form.data.get('perfil')).nome == 'SME' and
+                    User.objects.filter(perfil__nome='SME').count() == 3):
+                messages.set_level(request, messages.ERROR)
+                messages.error(request, 'Excedeu o limite de usuários SME no sistema.')
+                error = True
+        if not error:
+            super(UserAdmin, self).save_model(request, obj, form, change)
+
+
+admin.site.register(Perfil)
--- sme_ofertaimoveis/users/models.py
+++ sme_ofertaimoveis/users/models.py
@@ -1,17 +1,37 @@
 import uuid as uuid
 
 from django.contrib.auth.models import AbstractUser
-from django.contrib.auth.tokens import PasswordResetTokenGenerator
 from django.db import models
 from django.urls import reverse
+from rest_framework.exceptions import ValidationError
+
+
+class Perfil(models.Model):
+    nome = models.CharField("Nome", max_length=30, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.nome}"
+
+    class Meta:
+        verbose_name = "Perfil"
+        verbose_name_plural = "Perfis"
+
+
+class Secretaria(models.Model):
+    nome = models.CharField("Nome", max_length=30, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.nome}"
+
+    class Meta:
+        verbose_name = "Secretaria"
+        verbose_name_plural = "Secretarias"
 
 
 class User(AbstractUser):
     uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
-    # First Name and Last Name do not cover name patterns
-    # around the globe.
-    # name = CharField(_("Name of User"), blank=True, max_length=255)
+    perfil = models.ForeignKey(Perfil, null=True, on_delete=models.SET_NULL)
+    secretaria = models.ForeignKey(Secretaria, null=True, on_delete=models.SET_NULL)
 
     def get_absolute_url(self):
         return reverse("users:detail", kwargs={"username": self.username})
-
