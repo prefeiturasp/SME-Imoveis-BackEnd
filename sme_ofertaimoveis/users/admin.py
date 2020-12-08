945d042003ba7bcd1ee919af5d9f314dddd1db32
Calvin Rossinhole <calvin.masters@gmail.com>
2020-12-08 18:02:43 -0300

    ajustes

--- sme_ofertaimoveis/users/admin.py
+++ sme_ofertaimoveis/users/admin.py
@@ -18,10 +18,10 @@ class UserAdmin(admin.ModelAdmin):
                 messages.set_level(request, messages.ERROR)
                 messages.error(request, 'Excedeu o limite de usuários ADMIN no sistema.')
                 error = True
-            elif (Perfil.objects.get(id=form.data.get('perfil')).nome == 'SME' and
-                    User.objects.filter(perfil__nome='SME').count() == 3):
+            elif (Perfil.objects.get(id=form.data.get('perfil')).nome == 'SECRETARIA' and
+                    User.objects.filter(perfil__nome='SECRETARIA').count() == 3):
                 messages.set_level(request, messages.ERROR)
-                messages.error(request, 'Excedeu o limite de usuários SME no sistema.')
+                messages.error(request, 'Excedeu o limite de usuários SECRETARIA no sistema.')
                 error = True
         if not error:
             super(UserAdmin, self).save_model(request, obj, form, change)
