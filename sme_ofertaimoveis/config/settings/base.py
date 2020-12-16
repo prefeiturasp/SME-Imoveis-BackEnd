992ae2c09d160597f52e5a9ecff50f8d6fda4ebf
Calvin Rossinhole <calvin.masters@gmail.com>
2020-12-16 10:01:29 -0300

    liga debug

--- sme_ofertaimoveis/config/settings/base.py
+++ sme_ofertaimoveis/config/settings/base.py
@@ -20,7 +20,7 @@ if READ_DOT_ENV_FILE:
 # GENERAL
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#debug
-DEBUG = env.bool("DJANGO_DEBUG", False)
+DEBUG = True  # env.bool("DJANGO_DEBUG", False)
 
 # Local time zone. Choices are
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
