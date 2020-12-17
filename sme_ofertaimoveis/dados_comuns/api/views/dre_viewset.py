f1d8273189f2462c250e8194a6350971be1f822a
rodolphoazeredo <rodolpho.azeredo@amcom.com.br>
2020-12-17 19:33:42 -0300

    Remove paginação das DREs

--- sme_ofertaimoveis/dados_comuns/api/views/dre_viewset.py
+++ sme_ofertaimoveis/dados_comuns/api/views/dre_viewset.py
@@ -6,6 +6,7 @@ from ...models import DiretoriaRegional
 
 
 class DreViewset(mixins.ListModelMixin, GenericViewSet):
+    pagination_class = None
     permission_classes = (permissions.IsAuthenticated,)
     serializer_class = DreSerializer
     queryset = DiretoriaRegional.objects.all()
