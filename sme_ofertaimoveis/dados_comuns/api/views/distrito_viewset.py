1e57739f10eb7641bc5b137b385f29a7c9bd9c93
rodolphoazeredo <rodolpho.azeredo@amcom.com.br>
2021-01-14 13:00:28 -0300

    Feature: criar rota para buscar distritos por dre.

--- sme_ofertaimoveis/dados_comuns/api/views/distrito_viewset.py
+++ sme_ofertaimoveis/dados_comuns/api/views/distrito_viewset.py
@@ -1,8 +1,12 @@
 from rest_framework import mixins, permissions
 from rest_framework.viewsets import GenericViewSet
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
 
 from ..serializers import DistritoSerializer
-from ...models import Distrito
+from ...models import Distrito, Subprefeitura
 
 
 class DistritoViewset(mixins.ListModelMixin, GenericViewSet):
@@ -10,3 +14,21 @@ class DistritoViewset(mixins.ListModelMixin, GenericViewSet):
     permission_classes = (permissions.IsAuthenticated,)
     serializer_class = DistritoSerializer
     queryset = Distrito.objects.all()
+
+    @action(
+        detail=False,
+        methods=['GET'],
+        url_path=f'get_distritos_por_dre',
+        permission_classes=(IsAuthenticated,))
+    def get_distritos_por_dre(self, request):
+        subprefeituras = Subprefeitura.objects.filter(dre__id=request.query_params.get('dre'))
+        subprefeituras_ids = []
+        for subprefeitura in subprefeituras:
+            subprefeituras_ids.append(subprefeitura.id)
+
+        queryset = Distrito.objects.filter(subprefeitura__id__in=subprefeituras_ids)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(
+            data=serializer.data, status=status.HTTP_200_OK
+        )
