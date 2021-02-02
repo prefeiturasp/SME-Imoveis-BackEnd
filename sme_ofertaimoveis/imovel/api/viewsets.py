84c4eddcbf2a46d475d4c0e49db4614a33c2543e
rodolphoazeredo <rodolpho.azeredo@amcom.com.br>
2021-02-02 13:28:39 -0300

    Feature: Rotas para análise e encaminhamentos.

--- sme_ofertaimoveis/imovel/api/viewsets.py
+++ sme_ofertaimoveis/imovel/api/viewsets.py
@@ -344,6 +344,69 @@ class CadastroImoveisViewSet(viewsets.ModelViewSet,
             {'endereco_existe': endereco_existe}, status=status.HTTP_200_OK
         )
 
+    @action(detail=False,
+            methods=['POST'],
+            url_path='imoveis/envia-comapre',
+            permission_classes=(IsAuthenticated,))
+    def enviar_para_comapre(self, request):
+        imovel = Imovel.objects.get(id=request.query_params.get('imovel'))
+        if (request.query_params.get('enviar_email') == 'true'):
+            enviar_email = True
+        else:
+            enviar_email = False
+        data_agendada = request.query_params.get('data_agendada')
+        justificativa = ""
+        if 'justificativa' in request.query_params:
+            justificativa=request.query_params.get('justificativa')
+            user = request.user
+
+        imovel.sme_analisa_previamente(user=user, justificativa=justificativa)
+        imovel.envia_a_comapre(user=user, data_agendada=data_agendada, enviar_email=enviar_email)
+        serializer = self.get_serializer(imovel, context={'request': request})
+        return Response(status=status.HTTP_200_OK, data=serializer.data)
+
+    @action(detail=False,
+            methods=['POST'],
+            url_path='imoveis/finaliza-analise',
+            permission_classes=(IsAuthenticated,))
+    def finalizar_analise(self, request):
+        imovel = Imovel.objects.get(id=request.query_params.get('imovel'))
+        user = request.user
+        if (request.query_params.get('enviar_email') == 'true'):
+            enviar_email = True
+        else:
+            enviar_email = False
+        justificativa = ""
+        if 'justificativa' in request.query_params:
+            justificativa=request.query_params.get('justificativa')
+        imovel.sme_analisa_previamente(user=user, justificativa=justificativa)
+        if 'resultado_da_analise' in request.query_params:
+            if(request.query_params.get('resultado_da_analise') == '0'):
+                imovel.finaliza_area_insuficiente(user=user, enviar_email=enviar_email)
+            if(request.query_params.get('resultado_da_analise') == '1'):
+                imovel.finaliza_demanda_insuficiente(user=user, enviar_email=enviar_email)
+            if(request.query_params.get('resultado_da_analise') == '2'):
+                imovel.finaliza_nao_atende_necessidades(user=user, enviar_email=enviar_email)
+
+        serializer = self.get_serializer(imovel, context={'request': request})
+        return Response(status=status.HTTP_200_OK, data=serializer.data)
+
+    @action(detail=False,
+            methods=['POST'],
+            url_path='imoveis/agenda-vistoria',
+            permission_classes=(IsAuthenticated,))
+    def agendar_vistoria(self, request):
+        imovel = Imovel.objects.get(id=request.query_params.get('imovel'))
+        user = request.user
+        if (request.query_params.get('enviar_email') == 'true'):
+            enviar_email = True
+        else:
+            enviar_email = False
+        data_agendada = request.query_params.get('data_agendada')
+        imovel.agenda_vistoria(user=user, data_agendada=data_agendada, enviar_email=enviar_email)
+        imovel.aguarda_relatorio_vistoria(user=user)
+        serializer = self.get_serializer(imovel, context={'request': request})
+        return Response(status=status.HTTP_200_OK, data=serializer.data)
 
 class DemandaRegiao(APIView):
     """
